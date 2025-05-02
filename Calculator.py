e6654e90243c8a8904265c2647d59d01288c26a9
Muhammad Saad <akdcsaad918@gmail.com>
2025-05-02 15:12:23 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+#integer
+Quantity =12
+#float
+price_per_item=24.99
+#string
+item_name="Eggs"
+#total cost
+total_cost=Quantity*price_per_item
+#showing result
+print("I bought",Quantity, item_name +"s", "for a total of", total_cost,"rupees")
